8ce7ec9240d5a9b85ca69c83e31621958252db51
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2019-11-28 16:36:27 -0500

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+from thetechrobo import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='from thetechrobo import *',
+    version='v.0.3.2-plus',
+    packages=['cprint','exp'],
+    license='Artistic License',
+    long_description=open('README.txt').read(),
+)
