1596647bb6626a9f6957e007a811efc35fd2b00a
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2019-11-28 17:05:14 -0500

    OOPS

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='thetechrobo',
-    version='v.0.3.3-plus',
+    version='v.0.1',
     packages=['cprint','exp'],
     license='Artistic License',
     long_description="I add Literally any idea that pops into my head",
