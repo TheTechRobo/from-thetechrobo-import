e9545ecf7dcd1b5eb17bc40cb0a6673172171633
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2019-12-01 15:59:44 -0500

    __PRINTERROR:
    UNABLE TO ECHO THIS

--- exp/exp.py
+++ exp/exp.py
@@ -1,12 +1,13 @@
-def two(number):
-    print(number * number)
-def three(number):
-    print(number * number * number)
-def sq(number):
-    two(number)
-def cu(number):
-    three(number)
-def four(number):
-    print(number * number * number * number)
-def five(number):
-    print(number * number * number * number * number)
+class exp:
+    def two(number):
+        print(number * number)
+    def three(number):
+        print(number * number * number)
+    def sq(number):
+        two(number)
+    def cu(number):
+        three(number)
+    def four(number):
+        print(number * number * number * number)
+    def five(number):
+        print(number * number * number * number * number)
